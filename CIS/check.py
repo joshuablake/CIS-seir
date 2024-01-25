4444a44179088d3fed8121a83be16184f0bdb3d8
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2024-01-25 10:09:41 +0000

    working scripts to check runs completed successfully

--- CIS/check.py
+++ CIS/check.py
@@ -3,9 +3,11 @@ from covid19_seir.models import prevalence_model as model
 import numpy as np
 import pandas as pd
 
+i = 0
 for r in REGIONS:
     for c in range(NUM_CHAINS_PER_REGION):
         f = os.path.join(RESULTS_DIR, f"posterior_{r}_{c}.npy")
         # Read matrix of iteration x parameter, entries are values
         if not os.path.isfile(f):
-            print(f)
+            print(f, " ", i)
+        i += 1
